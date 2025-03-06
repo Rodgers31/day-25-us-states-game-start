886b247d8c03f5b54780e3fc6606afbe37974dfe
Rodgers31 <otienor986@gmail.com>
2025-03-05 18:45:04 -0600

    my solution

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ turtle.addshape(image)
 turtle.shape(image)
 
 write = Write()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?").title()
-print(answer_state)
 
-states = pandas.read_csv("50_states.csv")
-user_state = states[states["state"] == answer_state]
-the_state = str(user_state.state)
-the_x = user_state.x
-the_y = user_state.y
+while write.current_score < 50:
 
-if the_state:
-    write.write_state(answer_state, the_x, the_y)
-else:
-    pass
+    answer_state = screen.textinput(title=f"{write.current_score}/50 States correct",
+                                    prompt="What's another state's name?").title()
+
+    for state in all_states:
+        if answer_state == state:
+            user_state = data[data["state"] == state].to_dict()
+            for state_num in user_state["state"]:
+                state = user_state["state"][state_num]
+                state_x = user_state["x"][state_num]
+                state_y = user_state["y"][state_num]
+                write.write_state(state, state_x, state_y)
+        else:
+            pass
 
 screen.exitonclick()
--- write.py
+++ write.py
@@ -1,21 +1,22 @@
 from turtle import Turtle, Screen
 
 ALIGNMENT = "center"
-FONT = ("Courier", 13, "normal")
+FONT = ("Courier", 7, "normal")
+
+
 class Write(Turtle):
 
     def __init__(self):
         super().__init__()
         self.current_score = 0
         self.hideturtle()
+        self.penup()
 
     def write_state(self, state, st_x, st_y):
-        print("IN Write")
-        print(st_x)
-        print(st_y)
-        # self.goto(st_x, st_y)
-        # self.color("white")
-        # self.write(arg=f"{state}")
+        self.goto(st_x, st_y)
+        self.color("black")
+        self.write(arg=f"{state}", align=ALIGNMENT, font=FONT)
+        self.current_score += 1
 
 
 
